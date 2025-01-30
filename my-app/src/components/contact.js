48921315487e10cdce4609ecca82f38de15ff9b6
CleevePhilip <147903474+CleevePhilip@users.noreply.github.com>
2025-01-30 11:20:05 +0800

    Update contact.js

--- my-app/src/components/contact.js
+++ my-app/src/components/contact.js
@@ -57,7 +57,7 @@ const ContactSection = () => {
     try {
       // Send form data and captcha value to the backend
       await axios.post(
-        `https://portfolio-server-indol-rho.vercel.app/${formData.email}/${formData.name}/${formData.message}`,
+        `http://localhost:8000/${formData.email}/${formData.name}/${formData.message}`,
         {
           email: formData.email,
           name: formData.name,
